650942dd5e7e8eaca24f1b485e1becb8eb570a1b
The-Wizard-Chicken <46364879+The-Wizard-Chicken@users.noreply.github.com>
2019-01-04 16:27:40 +1000

    Add files via upload

---
+++ Password Username System.py
@@ -0,0 +1,45 @@
+while True:
+    print ("Enter your Username")
+    print ("\nIf you dont have an a account type 'r' to register")
+    inp = input(":")
+    if inp == "r":
+              print ("Enter your desired Username")
+              usr = input(":")
+              print ("Enter your desired Password")
+              pas = input(":")
+
+              print("Enter your Username")
+              us = input(":")
+              if us == usr:
+                        print ("Enter your Password")
+                        pus = input(":")
+              elif us != usr:
+                        print("Incorrect Username")
+                        import os
+                        os.system('pause')
+                        break
+              if pus == pas:
+                        print ("Succesfully logged in")
+                        import os
+                        os.system('pause')
+                        break
+              elif pus != pas:
+                        print("Incorrect Password")
+                        import os
+                        s.system('pause')
+                        break
+              if inp == usr:
+                        print ("Enter your Password")
+                        inp = input(":")
+              elif inp != usr:
+                        print ("Incorrect Username")
+                        import os
+                        os.system('pause')
+                        break
+              if inp == pas:
+                        print (" Succesfully logged in")
+              elif inp != pas:
+                        print ("Incorrect Password")
+                        import os
+                        os.system('pause')
+                        break
